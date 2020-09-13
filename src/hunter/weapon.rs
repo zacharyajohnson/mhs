519a24e61d280999847e29b64454235466400e4d
Zachary Alan Johnson <Zjohnson366@gmail.com>
2020-09-13 17:39:14 -0400

    Commented out weapon.rs for later work

--- src/hunter/weapon.rs
+++ src/hunter/weapon.rs
@@ -1,37 +1,37 @@
-pub enum Type {
-    GreatSword,
-    LongSword,
-    SwordAndShield,
-    Hammer,
-    DualBlades,
-    SwitchAxe,
-    Lance,
-    GunLance,
-    HuntingHorn
-}
+// pub enum Type {
+//     GreatSword,
+//     LongSword,
+//     SwordAndShield,
+//     Hammer,
+//     DualBlades,
+//     SwitchAxe,
+//     Lance,
+//     GunLance,
+//     HuntingHorn
+// }
 
-impl Type {
-    pub fn as_str(&self) -> &'static str {
-        use Type::*;
-        match *self { 
-            GreatSword => "Great Sword",
-            LongSword => "Long Sword",
-            SwordAndShield => "Sword and Shield",
-            Hammer => "Hammer",
-            _ => ""
-        }
-    }
+// impl Type {
+//     pub fn as_str(&self) -> &'static str {
+//         use Type::*;
+//         match *self {
+//             GreatSword => "Great Sword",
+//             LongSword => "Long Sword",
+//             SwordAndShield => "Sword and Shield",
+//             Hammer => "Hammer",
+//             _ => ""
+//         }
+//     }
 
-    pub fn from_str(string: &str) -> Type {
-        use Type::*;
+//     pub fn from_str(string: &str) -> Type {
+//         use Type::*;
 
-        match string {
-            "Great Sword" => GreatSword,
-            "Long Sword" => LongSword,
-            "Sword and Shield" =>SwordAndShield, 
-            "Hammer" => Hammer,
-            _ => Lance
-            
-        }
-    }
-}
+//         match string {
+//             "Great Sword" => GreatSword,
+//             "Long Sword" => LongSword,
+//             "Sword and Shield" =>SwordAndShield,
+//             "Hammer" => Hammer,
+//             _ => Lance
+
+//         }
+//     }
+// }
