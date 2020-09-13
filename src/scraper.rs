2a3660c60c26fb0a8c0cc2f326b7c45753885593
Zachary Alan Johnson <Zjohnson366@gmail.com>
2020-09-13 17:35:58 -0400

    Refactored how the parser is pulling back basic game info from monster hunter fandom

--- src/scraper.rs
+++ src/scraper.rs
@@ -1,85 +1,108 @@
-use reqwest::{Response};
+// use regex::{Regex, RegexBuilder};
+use reqwest::Response;
 use select::document::Document;
-use select::predicate::{Name};
-use std::collections::HashSet;
-use std::fs::File;
-use regex::{RegexBuilder, Regex};
+use select::predicate::Name;
+use select::predicate::Class;
+use std::collections::{BTreeSet, HashSet};
 
 mod monster_hunter_fandom;
 use monster_hunter_fandom::URLS;
 
 mod kiranico;
-
-use super::game::Game;
+use super::game::*;
 
 // Somehow cache these values in memory and in a file so we don't have to reach out via an HTTP request all the time.
+// TODO These implementation details are starting to become specific to monster_hunter_fandom. I should create a wrapper around this.
 pub fn get_games() -> HashSet<Game> {
-      let resp: Response = reqwest::get(&URLS.get_game_list_url()).unwrap();
+    let mut games: HashSet<Game> = HashSet::new();
 
-        assert!(resp.status().is_success(), "Url {} was not successfully retrieved by client, error code was {}", URLS.get_game_list_url(),resp.status());
-
-
-        let resp_document: Document = Document::from_read(resp).unwrap();
-        
-        let _file = File::create("C:\\Users\\zjohnson\\Documents\\urls.txt").expect("Failed to create file");
-
-        let game_wiki_regex: Regex = RegexBuilder::new(r"Monster.*Hunter")
-            .case_insensitive(false)
-            .build()
-            .unwrap();
-
-        let mut games: HashSet<Game> = HashSet::new();
-        for node in resp_document.find(Name("a")) {
-            let attributes: Vec<(&str, &str)> = node.attrs().filter(|x| game_wiki_regex.is_match(x.1)).collect();
-
-            if attributes.len() == 2 {
-                let mut absolute_url: String = URLS.get_base_url();
-                absolute_url.push_str(attributes[0].1);
-                let title: String =  String::from(attributes[1].1);
-                let game: Game = Game{title, absolute_url};
-
+    for supported_game in SUPPORTED_GAMES {
+        match get_game(supported_game) {
+            Some(game) => {
                 games.insert(game);
-
-                //println!("{:?}", attributes);
-            } else {
-                continue;
             }
+            None => panic!("Cannot find info for {}", supported_game),
         }
-        games
+    }
+
+    games
 }
 
-pub fn get_game(game_title: &str) -> Game {
-    let games: HashSet<Game> = get_games();
+pub fn get_game(game_title: &str) -> Option<Game> {
+    if !SUPPORTED_GAMES.contains(&game_title) {
+        return Option::None;
+    }
 
-    let game: &Game = games.iter().find(|x| x.title == game_title).expect("Could not find game");
-    Game{title: String::from(&game.title), absolute_url: String::from(&game.absolute_url)}
+    let mut game_url: String = String::from(URLS.base_url);
+    let game_title_url: String = game_title.replace(" ", "_");
+    game_url.push_str(&game_title_url);
 
+    let resp: Response = reqwest::get(&game_url).unwrap();
+
+    assert!(
+        resp.status().is_success(),
+        "Url {} was not successfully retrieved by client, error code was {}",
+        game_url,
+        resp.status()
+    );
+
+    let resp_document: Document = Document::from_read(resp).unwrap();
+
+    for node in resp_document.find(Class("pi-data")) {
+        println!("{:?}", node);
+    }
+
+    let mut titles: BTreeSet<String> = BTreeSet::new();
+
+    if let Some(alternative_title) = get_alternative_titles(&game_title) {
+        titles.insert(String::from(alternative_title));
+    }
+
+    titles.insert(String::from(game_title));
+
+    let mut absolute_urls: BTreeSet<String> = BTreeSet::new();
+    absolute_urls.insert(game_url);
+
+    Option::Some(Game {
+        titles,
+        absolute_urls,
+    })
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*; 
+    use super::*;
+    use std::iter::Iterator;
 
     #[test]
     fn get_games_list_test() {
         let games: HashSet<Game> = get_games();
 
+        // We should at least return something
         assert_ne!(0, games.len());
-        // Checks to see if the list returns at least a game in each generation of the series
-        // If we can't at least find these we are in big trouble lol
-        assert_ne!(games.iter().find(|x| x.title == "Monster Hunter"), None);
-        assert_ne!(games.iter().find(|x| x.title == "Monster Hunter 2"), None);
-        assert_ne!(games.iter().find(|x| x.title == "Monster Hunter 3"), None);
-        assert_ne!(games.iter().find(|x| x.title == "Monster Hunter 4"), None);
-        assert_ne!(games.iter().find(|x| x.title == "Monster Hunter: World"), None);
+
+        // Checks to see if we have all games we support by title.
+        for supported_game in SUPPORTED_GAMES {
+            assert_ne!(
+                games.iter().find(|x| x.titles.contains(*supported_game)),
+                None
+            );
+        }
+
+        // Make sure that the list we are returning is the same length as the amount of games we support
+        assert_eq!(SUPPORTED_GAMES.len(), games.len());
     }
 
     #[test]
     fn get_game_test() {
         let game_title: &str = "Monster Hunter";
-        let game: Game = get_game(game_title);
-        assert_eq!(game_title, game.title, "Test Failed. Searched for game {} and got {}", game_title, game.title);
+        let game: Game = get_game(game_title).unwrap();
+        
+        assert!(
+            game.titles.contains(game_title),
+            "Test Failed. Searched for game {} and got {}",
+            game_title,
+            game
+        );
     }
 }
-
- 
