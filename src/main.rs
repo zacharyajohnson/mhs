79c5db2873467acf7fdfd815eab91ea4b50b53d4
Zachary Alan Johnson <Zjohnson366@gmail.com>
2020-09-13 17:34:34 -0400

    Updated main to only print command line arguments and to get the first game

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
 use std::env;
 
-mod gui;
 mod cli;
 mod game;
+mod gui;
 
-mod parser;
 mod hunter;
+mod scraper;
 
 fn main() {
     let cli_arguments: Vec<String> = env::args().collect();
 
-    //if cli_arguments.len() == 0 { 
-      //  gui::start_gui();
-    //} else {
-        for argument in &cli_arguments {
-            println!("{}" , argument);
-        }
+    for argument in &cli_arguments {
+        println!("{}", argument);
+    }
 
-        let command: String = cli_arguments.get(1).unwrap().to_string();
-        //let command_arguments = cli_arguments.get(2..).iter().map(|x| )
-        
-        //cli::process_command(&command, command_arguments);
-//    }
-
+    scraper::get_game("Monster Hunter");
+  
 }
-
-
